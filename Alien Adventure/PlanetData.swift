50e07d17ef35a2c3e1f5ec921e3f928e814f3973
Tiago-Rocha <tiago.aziz@gmail.com>
2016-07-25 23:40:29 +0200

    Planet Data class cleaned

--- Alien Adventure/PlanetData.swift
+++ Alien Adventure/PlanetData.swift
@@ -15,40 +15,34 @@ extension Hero {
         let rawPlanetsJSON = NSData(contentsOfURL: planetsInfoJSON)
         var maxTotal = 0
         var maxPlanet = ""
-    
+        
         let planetsArray: [[String: AnyObject]]!
         do {
             planetsArray = try! NSJSONSerialization.JSONObjectWithData(rawPlanetsJSON!, options: NSJSONReadingOptions()) as! [[String: AnyObject]]
         }
         for planet in planetsArray! {
-                var total = 0
-                if let common = planet["CommonItemsDetected"] {
-                    total += common as! Int * 1
-                }
-                if let uncommon = planet["UncommonItemsDetected"] {
-                    total += uncommon as! Int * 2
-                }
-                if let rare = planet["RareItemsDetected"] {
-                    total += rare as! Int * 3
-                }
-                if let legendary = planet["LegendaryItemsDetected"] {
-                    total += legendary as! Int * 4
-                }
+            var total = 0
+            if let common = planet["CommonItemsDetected"] {
+                total += common as! Int * 1
+            }
+            if let uncommon = planet["UncommonItemsDetected"] {
+                total += uncommon as! Int * 2
+            }
+            if let rare = planet["RareItemsDetected"] {
+                total += rare as! Int * 3
+            }
+            if let legendary = planet["LegendaryItemsDetected"] {
+                total += legendary as! Int * 4
+            }
             
             if total > maxTotal {
                 maxPlanet = planet["Name"] as! String
                 maxTotal = total
             }
-                print("\(planet["Name"]) has \(total) points)")
-            }
-        /*
-        "Name": "Preshan",
-        "CommonItemsDetected": 77, - 1
-        "UncommonItemsDetected": 33, - 2
-        "RareItemsDetected": 20, - 3
-        "LegendaryItemsDetected": 8 - 4
-         */
-                return maxPlanet
+            print("\(planet["Name"]) has \(total) points)")
+        }
+        
+        return maxPlanet
     }
 }
 
